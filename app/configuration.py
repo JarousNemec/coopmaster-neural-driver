4ca0740dfd3a470b839257eea8bbf9fdefa8aab1
JarousNemec <jarous.nemec@gmail.com>
2024-10-18 22:14:22 +0200

    setupped default template

---
+++ app/configuration.py
@@ -0,0 +1,13 @@
+import os
+
+host = "127.0.0.1"
+port = 9002
+hello_message = "Hello from neural driver"
+
+log_file_name = "neural_driver.log"
+
+def get_log_directory():
+    return "./logs/"
+
+def get_log_filename():
+    return log_file_name;
